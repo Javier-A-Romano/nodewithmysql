d69d3e416b7a8598e1b6c740bd40659019dc0690
Javier Romano <romanojavier92@gmail.com>
2022-11-06 10:33:24 -0300

    first config mysql

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import express from 'express'
 
 const app = express()
+const PORT = process.env.PORT || 3000
+app.get('/employes', (req,res)=> res.send('get employe'))
+app.post('/employes', (req,res)=> res.send('created employe'))
+app.put('/employes', (req,res)=> res.send('update employe'))
+app.delete('/employes', (req,res)=> res.send('deleted employe'))
 
-
-
-app.listen(3000)
+app.listen(PORT, function () {
+  console.log("Server listening in " + PORT)
+});
