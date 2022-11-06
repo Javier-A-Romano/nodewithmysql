65bf6a485a9d6ae2cb8949508d04c7f52e0d8611
Javier Romano <romanojavier92@gmail.com>
2022-11-06 11:27:10 -0300

    get All data

--- src/controllers/employees.controller.js
+++ src/controllers/employees.controller.js
@@ -1,10 +1,16 @@
 import { pool } from '../db.js'
 
-export const getEmployee = (req, res) => res.send('get employe')
+export const getEmployee = async (req, res) => {
+    const [rows]=await pool.query('SELECT * FROM employee')
+    res.json(rows)
+}
 export const createEmployee = async(req, res) => {
     const {name,salary} = req.body
     const [rows]=await pool.query('INSERT INTO employee (name,salary) VALUES (?,?)', [name , salary])
-    res.send(rows)
+    res.send({
+        id : rows.insertId,
+        name,salary
+    })
 }
 export const updateEmployee = (req, res) => res.send('update employe')
 export const deleteEmployee = (req, res) => res.send('delete employe')
